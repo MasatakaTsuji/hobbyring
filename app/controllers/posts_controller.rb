3c143938c5a1b6747d40ffd2d5a235bc7e81b9ac
MasatakaTsuji <mstk73@yahoo.co.jp>
2020-11-05 18:05:22 +0900

    deviseを導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,23 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+  end
   
+  def create
+    @post = Post.new(post_params)
+    if @post.vaild?
+      @post.seve
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  # def post_params
+  #   params.require(:post).permit().merge(user_id: currnt_user.id)
+  # end
 end
